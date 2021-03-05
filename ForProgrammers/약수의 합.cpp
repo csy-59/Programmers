5da1ab05c746bea178b8379ace0321bcfce989f7
csy-59 <csy010509@naver.com>
2021-03-05 21:39:59 +0900

    1. setting default setup

---
+++ ForProgrammers/약수의 합.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(int n) {
+	int answer = 0;
+
+	return answer;
+}
+
+void main() {
+	int n = 12;
+	cout << solution(n) << endl;
+}
