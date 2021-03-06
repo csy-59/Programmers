7e223f1a4a08aaddc01e88abf9868d6148cb51d9
csy-59 <csy010509@naver.com>
2021-03-06 20:41:13 +0900

    자연수 문제 틀

---
+++ ForProgrammers/자연수 뒤집어 배열로 만들기.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<int> solution(long long n) {
+	vector<int> answer;
+
+	return answer;
+}
+
+void main() {
+	long long n = 12345;
+	vector<int> result = solution(n);
+	for (unsigned i = 0;i < result.size();i++) {
+		cout << result[i] << " ";
+	}
+}
